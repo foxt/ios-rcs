da000b113a931d61b6c04f0f211e84ae48def4a3
foxt <me@foxt.dev>
2024-10-01 17:14:20 +0100

    wat

--- buildHtml.tsx
+++ buildHtml.tsx
@@ -47,7 +47,7 @@ const CarrierSupportTable = () => {
     </>))}</div>
 
 }
-import { Fragment, h } from "preact";
+console.log({Fragment});
 let html = renderToString(<>
     <head>
         <title>Does My Carrier Support RCS on iOS Yet?</title>
